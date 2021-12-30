a95be71f0d5595a2682c0c67e08122f8831e3b1e
Pavel Schoffer <xschof00@gmail.com>
2021-12-30 08:25:21 +0100

    coolness

--- contracts/BeaverNFT.sol
+++ contracts/BeaverNFT.sol
@@ -7,11 +7,21 @@ contract BeaverNFT is ERC721 {
 
     uint256 public totalSupply;
 
+    mapping(uint256 => string) private _tokenUris;
+
     constructor() ERC721("BeaverNFT","BNFT") {
     }
 
     function mint() public {
         _mint(msg.sender, totalSupply);
         totalSupply += 1;
+    }
+
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        return _tokenUris[tokenId];
+    }
+
+    function setTokenURI(uint256 tokenId, string memory tokenUri) external {
+        _tokenUris[tokenId] = tokenUri
     }
 }
