11dcd29fdf3a4fdc6ebfc867b641d9dff728696d
Pavel Schoffer <xschof00@gmail.com>
2021-12-30 15:10:21 +0100

    stuff

--- contracts/BeaverNFT.sol
+++ contracts/BeaverNFT.sol
@@ -2,6 +2,7 @@ pragma solidity 0.8.10;
 
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract BeaverNFT is ERC721 {
 
@@ -13,5 +14,10 @@ contract BeaverNFT is ERC721 {
     function mint() public {
         _mint(msg.sender, totalSupply);
         totalSupply += 1;
+    }
+
+    function tokenURI(uint256 tokenId) public pure override returns (string memory) {
+        return string(abi.encodePacked("https://raw.githubusercontent.com/beaver-codes/BeaverNFT/master/assets/beaverNFT_", Strings.toString(tokenId) , ".json"));
+
     }
 }
